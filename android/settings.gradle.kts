f75779fce2d42e43a7946ecf20ba5adcac98bf6f
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-07-02 15:10:45 +0000

    Update Flutter Gradle plugin comment in settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false // ✅ Obligatoire ici
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false // 👈 LA LIGNE 17 COMPORTANT LE "apply false"
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
