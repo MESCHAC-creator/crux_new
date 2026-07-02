bac748a0e54641cb28333fe97f62685136791513
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-07-02 12:51:25 +0000

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -15,7 +15,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" // <-- Activé sans "apply false" pour charger les plugins !
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
