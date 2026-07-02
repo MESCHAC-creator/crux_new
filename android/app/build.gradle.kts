2071b13bee04ae582d4e32ec33d98f188067adf6
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-07-02 12:23:27 +0000

    Update build.gradle.kts for Android configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,45 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.crux"
+    compileSdk = 34
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    defaultConfig {
+        applicationId = "com.example.crux"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+        
+        multiDexEnabled = true
+    }
+
     buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false      // Désactive l'optimisation du code
-            isShrinkResources = false    // <-- AJOUTE CETTE LIGNE pour corriger l'erreur !
-            
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+}
+
+dependencies {
+    implementation("androidx.multidex:multidex:2.0.1")
+}
