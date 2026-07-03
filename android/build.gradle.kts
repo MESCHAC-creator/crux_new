ad64767184571a17024ba6db0f0c1e8582fd7959
MESCHAC-creator <kouakouchristevann@gmail.com>
2026-07-03 16:53:09 +0000

    Update compileSdkVersion and targetSdkVersion to 35

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,17 +7,15 @@ allprojects {
 
 // Ces propriétés seront lues par les plugins comme app_links
 subprojects {
-    project.extra.set("compileSdkVersion", 34)
+    project.extra.set("compileSdkVersion", 35)
     project.extra.set("minSdkVersion", 21)
     project.extra.set("targetSdkVersion", 34)
 }
 
 rootProject.layout.buildDirectory.set(rootProject.layout.projectDirectory.dir("../build"))
-
 subprojects {
     project.layout.buildDirectory.set(rootProject.layout.buildDirectory.dir(project.name))
 }
-
 subprojects {
     project.evaluationDependsOn(":app")
 }
